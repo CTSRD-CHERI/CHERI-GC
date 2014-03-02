6f3ec409abb1979d6a37c734c8f9aeb249af8835
Munraj Vadera <munraj@poolcue>
2014-03-02 17:19:37 +0000

    Added assembly output to Makefile.CHERI and capability register pushing to gc_collect.c.

--- src/gc_collect.c
+++ src/gc_collect.c
@@ -6,6 +6,8 @@
 #include <machine/cheri.h>
 #include <machine/cheric.h>
 
+#include <stdint.h>
+
 void
 GC_collect (void)
 {
@@ -31,6 +33,8 @@ GC_reallocate_roots (struct GC_region * region)
   return 0;
 }
 
+typedef __capability void * GC_cap_ptr;
+
 int
 GC_get_roots (struct GC_region * region)
 {
@@ -47,15 +51,69 @@ GC_get_roots (struct GC_region * region)
       region->roots[region->nroots++].cap = (x); \
     } while (0)
   
+  #define GC_PUSH_CAP_REG(cap_reg,dest_addr) \
+    __asm __volatile \
+    ( \
+      "csc $c" #cap_reg ", %0, 0($c0)" : : "r"(dest_addr) : "memory" \
+    )
+  
+  #define GC_ALIGN_32(ptr,typ) \
+    do { \
+      (ptr) = \
+        (typ) ( (((uintptr_t) (ptr)) + (uintptr_t) 31) & ~(uintptr_t) 0x1F ); \
+    } while (0)
+   
+  __capability void * cap_regs_misaligned[sizeof(__capability void *)*26+32];
+  GC_cap_ptr * cap_regs = cap_regs_misaligned;
+  GC_ALIGN_32(cap_regs, GC_cap_ptr *); // CSC instruction needs 32-byte aligned
+                                       // destination address.
+  GC_PUSH_CAP_REG(1, &cap_regs[0]);
+  GC_PUSH_CAP_REG(2, &cap_regs[1]);
+  GC_PUSH_CAP_REG(3, &cap_regs[2]);
+  GC_PUSH_CAP_REG(4, &cap_regs[3]);
+  GC_PUSH_CAP_REG(5, &cap_regs[4]);
+  GC_PUSH_CAP_REG(6, &cap_regs[5]);
+  GC_PUSH_CAP_REG(7, &cap_regs[6]);
+  GC_PUSH_CAP_REG(8, &cap_regs[7]);
+  GC_PUSH_CAP_REG(9, &cap_regs[8]);
+  GC_PUSH_CAP_REG(10, &cap_regs[9]);
+  GC_PUSH_CAP_REG(11, &cap_regs[10]);
+  GC_PUSH_CAP_REG(12, &cap_regs[11]);
+  GC_PUSH_CAP_REG(13, &cap_regs[12]);
+  GC_PUSH_CAP_REG(14, &cap_regs[13]);
+  GC_PUSH_CAP_REG(15, &cap_regs[14]);
+  GC_PUSH_CAP_REG(16, &cap_regs[15]);
+  GC_PUSH_CAP_REG(17, &cap_regs[16]);
+  GC_PUSH_CAP_REG(18, &cap_regs[17]);
+  GC_PUSH_CAP_REG(19, &cap_regs[18]);
+  GC_PUSH_CAP_REG(20, &cap_regs[19]);
+  GC_PUSH_CAP_REG(21, &cap_regs[20]);
+  GC_PUSH_CAP_REG(22, &cap_regs[21]);
+  GC_PUSH_CAP_REG(23, &cap_regs[22]);
+  GC_PUSH_CAP_REG(24, &cap_regs[23]);
+  GC_PUSH_CAP_REG(25, &cap_regs[24]);
+  GC_PUSH_CAP_REG(26, &cap_regs[25]);
+  
+  {
+  int i;
+  for (i=0; i<26; i++)
+    GC_dbgf("cap_reg root [%d]: t=%d, b=0x%llx, l=0x%llx\n",
+      i,
+      (int) cheri_gettag(cap_regs[i]),
+      (unsigned long long) cheri_getbase(cap_regs[i]),
+      (unsigned long long) cheri_getlen(cap_regs[i])
+    );
+  }
+ 
   // TODO:
+  // for every cap:
   // load into r1 &cap_ptr
   // csc $cX, $r1, 0($c0)
-  __asm __volatile
-  (
-    "csc $1, $c1" : :  : "$1"
-  );
-  
-  
+  //__asm __volatile
+  //(
+  //  "csc $c1, %0, 0($c0)" : : "r"(region->nroots) : "memory"
+  //);
+    
   // no, no, no. this needs to all be done in assembly or something so that we
   // can ensure that registers aren't modified in the process.
   
