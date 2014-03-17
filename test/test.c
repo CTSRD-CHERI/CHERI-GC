2f2a334771a023558b4c93d1dddab6bc9efd46af
Munraj Vadera <munraj@poolcue>
2014-03-17 18:35:13 +0000

    Added testing stuff for Boehm.

--- test/test.c
+++ test/test.c
@@ -1,3 +1,5 @@
+#ifdef GC_CHERI
+
 #include <gc_init.h>
 #include <gc_malloc.h>
 #include <gc_debug.h>
@@ -6,6 +8,23 @@
 
 #include <machine/cheri.h>
 #include <machine/cheric.h>
+
+#else // GC_CHERI
+
+// Boehm
+
+#define GC_ULL  unsigned long long
+typedef void * GC_cap_ptr;
+#define __capability
+#define GC_ALIGN_32(x,typ)  ((typ)(x))
+void __LOCK_MALLOC ()
+{
+}
+void __UNLOCK_MALLOC ()
+{
+}
+
+#endif // GC_CHERI
 
 #include <stdio.h>
 #include <string.h>
@@ -17,7 +36,14 @@ collection_test (void);
 int
 main (int argc, char **argv)
 {
-  printf("test: compiled %s\n", __TIME__ " " __DATE__);
+  printf("test: compiled %s using %s\n",
+         __TIME__ " " __DATE__,
+#ifdef GC_CHERI
+         "GC2/CHERI"
+#else // GC_CHERI
+         "Boehm"
+#endif // GC_CHERI
+         );
   collection_test();
   return 0;
 }
@@ -52,18 +78,23 @@ collection_test (void)
     "G" \
   )
   
+#ifdef GC_CHERI
   GC_init();
-  
+  GC_ULL semispace_size = GC_cheri_getlen(GC_state.thread_local_region.tospace);
+#else // GC_CHERI
+  GC_ULL semispace_size = GC_get_heap_size();
+#endif // GC_CHERI
+ 
   printf("Semi-space size         : 0x%llx (%llu) byte (~%llu%s)\n"
          "Number of allocations   : 0x%llx (%llu)      (~%llu%s)\n"
          "Size of each allocation : 0x%llx (%llu) byte (~%llu%s)\n"
          "Total allocation        : 0x%llx (%llu) byte (~%llu%s)\n"
          "Array entries           : 0x%llx (%llu)      (~%llu%s)\n"
          "Retained contents       : 0x%llx (%llu) byte (~%llu%s)\n",
-         (GC_ULL) GC_cheri_getlen(GC_state.thread_local_region.tospace),
-         (GC_ULL) GC_cheri_getlen(GC_state.thread_local_region.tospace),
-         (GC_ULL) MEM_PRETTY(GC_cheri_getlen(GC_state.thread_local_region.tospace)),
-         MEM_PRETTY_UNIT(GC_cheri_getlen(GC_state.thread_local_region.tospace)),
+         (GC_ULL) semispace_size,
+         (GC_ULL) semispace_size,
+         (GC_ULL) MEM_PRETTY(semispace_size),
+         MEM_PRETTY_UNIT(semispace_size),
          (GC_ULL) num_alloc,
          (GC_ULL) num_alloc,
          (GC_ULL) NUM_PRETTY(num_alloc),
@@ -102,8 +133,22 @@ collection_test (void)
     }
   }
   after = time(NULL);
+  
+#ifdef GC_CHERI
+  int num_collections = GC_state.num_collections;
+#else // GC_CHERI
+  int num_collections = 0;
+#endif // GC_CHERI
+  
   printf("Time taken: %llu sec\n"
          "Num collections: %llu\n",
          (GC_ULL) (after - before),
-         (GC_ULL) GC_state.num_collections);
+         (GC_ULL) num_collections);
+
+#ifdef GC_CHERI
+#else // GC_CHERI
+  // Boehm
+  printf("Boehm heap size now: 0x%llx\n", GC_get_heap_size());
+#endif // GC_CHERI
+  
 }
