7799220c856e7a4e9bda28cb02d16c6a1e638c8e
Munraj Vadera <munraj@poolcue>
2014-05-04 11:27:43 +0100

    More results.

--- test/testall.sh
+++ test/testall.sh
@@ -1,11 +1,11 @@
 # this script is pushed to and run on cloud2 by Makefile.testall
-prog=/mnt/mbv21zfs/testall
+prog=/mnt/mbv21zfs/testall-boehm
 $prog 0
 i=1
 while [ $i -le 20 ]
 do
   j=1
-  while [ $j -le 10 ]
+  while [ $j -le 3 ]
   do
     $prog $i data
     echo testall.sh: i=$i j=$j
